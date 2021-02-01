cc3f430d87e4066b5e10f48c22588e94909e2f97
rickytorrez <torrez.ricky@ymail.com>
2021-02-01 14:25:29 -0500

    Rating Component, f-awsm star icon, styling

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Rating from '../components/Rating';
 import { Card } from 'react-bootstrap';
 
 const Product = ({ product }) => {
@@ -15,14 +16,19 @@ const Product = ({ product }) => {
 					</Card.Title>
 				</a>
 				<Card.Text as='div'>
-					<div className='my-3'>
-						{product.rating} from {product.numReviews} reviews.
-					</div>
+					<Rating
+						value={product.rating}
+						text={`${product.numReviews} reviews`}
+					/>
 				</Card.Text>
 				<Card.Text as='h3'>${product.price}</Card.Text>
 			</Card.Body>
 		</Card>
 	);
+};
+
+Rating.defaultProps = {
+	color: '#f8e825',
 };
 
 export default Product;
