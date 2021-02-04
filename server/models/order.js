c3b15f8522b3c084db9b3bc4450f8c5556514a83
rickytorrez <torrez.ricky@ymail.com>
2021-02-04 18:51:37 -0500

    Modeling data

---
+++ server/models/order.js
@@ -0,0 +1,114 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+		// array of order items
+		// the order will have a relationship to the product
+		orderItems: [
+			{
+				name: {
+					type: String,
+					required: true,
+				},
+				quantity: {
+					type: Number,
+					required: true,
+				},
+				image: {
+					type: String,
+					required: true,
+				},
+				price: {
+					type: number,
+					required: true,
+				},
+				// relationship
+				product: {
+					type: mongoose.Schema.Types.ObjectId,
+					required: true,
+					ref: 'Product',
+				},
+			},
+		],
+		shippingAdress: {
+			// embedded objects
+			address: {
+				type: String,
+				required: true,
+			},
+			city: {
+				type: String,
+				required: true,
+			},
+			postalCode: {
+				type: String,
+				required: true,
+			},
+			country: {
+				type: String,
+				required: true,
+			},
+		},
+		paymentMethod: {
+			type: String,
+			required: true,
+		},
+		paymentResult: {
+			id: {
+				type: String,
+			},
+			status: {
+				type: String,
+			},
+			update_time: {
+				type: String,
+			},
+			email_address: {
+				type: String,
+			},
+		},
+		taxPrice: {
+			type: Number,
+			required: true,
+			default: 0.0,
+		},
+		shipingPrice: {
+			type: Number,
+			required: true,
+			default: 0.0,
+		},
+		totalPrice: {
+			type: Number,
+			required: true,
+			default: 0.0,
+		},
+		isPaid: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		paidAt: {
+			type: Date,
+		},
+		isDelivered: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		deliveredAt: {
+			type: Date,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
