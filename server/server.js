119f1f45c0d93c7e003b3e4271c748a388bd2dd3
rickytorrez <torrez.ricky@ymail.com>
2021-02-05 11:34:37 -0500

    Initial product routes, fetching from DB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 // imports changed through ES Modules
 import express from 'express';
 import dotenv from 'dotenv';
-import colors from 'colors';
 import connectDB from './config/db.js';
-import products from './fixtures/products.js';
+import color from 'colors';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -15,14 +15,7 @@ app.get('/', (req, res) => {
 	res.send('API is running..f.');
 });
 
-app.get('/api/products', (req, res) => {
-	res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((product) => product._id === req.params.id);
-	res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
