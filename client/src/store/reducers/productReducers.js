63558ef30f9ebc1700e214ffd9d95612d7331dc6
rickytorrez <torrez.ricky@ymail.com>
2021-03-23 19:22:49 -0400

    single product action, reducer, constant

--- client/src/store/reducers/productReducers.js
+++ client/src/store/reducers/productReducers.js
@@ -2,9 +2,15 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
-/** handles the state for the prodcut list on the home page */
+/**
+ * handles the state for the product list on the home page
+ * take state as params, initially products, an empty array and an action
+ */
 export const productListReducer = (state = { products: [] }, action) => {
 	switch (action.type) {
 		case PRODUCT_LIST_REQUEST:
@@ -13,6 +19,30 @@ export const productListReducer = (state = { products: [] }, action) => {
 		case PRODUCT_LIST_SUCCESS:
 			return { loading: false, products: action.payload };
 		case PRODUCT_LIST_FAIL:
+			return { loading: false, error: action.payload };
+		default:
+			return state;
+	}
+};
+
+/**
+ * handles the state for the product details on the single product page
+ * takes state as param, a product object - take reviews as a part of the object witch an empty array being passed and an action
+ */
+export const productDetailsReducer = (
+	state = { product: { reviews: [] } },
+	action
+) => {
+	switch (action.type) {
+		case PRODUCT_DETAILS_REQUEST:
+			/**
+			 * set loading to true
+			 * add whatever is on the state to the request
+			 */
+			return { loading: true, ...state };
+		case PRODUCT_DETAILS_SUCCESS:
+			return { loading: false, product: action.payload };
+		case PRODUCT_DETAILS_FAIL:
 			return { loading: false, error: action.payload };
 		default:
 			return state;
