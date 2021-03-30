a4ff3491f8fa3eab083753b8c22a9827c8fb151e
rickytorrez <torrez.ricky@ymail.com>
2021-03-29 21:59:04 -0400

    user controller, route and mounting on server file

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import color from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-	res.send('API is running..f.');
+	res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 /** middleware */
 app.use(notFound);
