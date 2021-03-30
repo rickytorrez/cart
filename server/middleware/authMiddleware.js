2da498893649ef70616a1a69f162af28b6edc5a9
rickytorrez <torrez.ricky@ymail.com>
2021-03-29 22:45:51 -0400

    auth middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,48 @@
+/**
+ * middleware to validate the token
+ */
+
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/user.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+	let token;
+
+	/**
+	 * conditionally check to see if there's an authorization token in the request headers
+	 * check for the word Bearer on the request
+	 */
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		/**
+		 * if true, split the authorization and take the second index, the token
+		 */
+		try {
+			token = req.headers.authorization.split(' ')[1];
+			/**
+			 * decode the token by verifying it being compared to the process.env.JWT_SECRET
+			 */
+			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+			/**
+			 * find the user by the decoded.id
+			 */
+			req.user = await User.findById(decoded.id).select('-password');
+			next();
+		} catch (err) {
+			console.error(err);
+			res.status(401);
+			throw new Error('Not authorized, token failed');
+		}
+	}
+
+	if (!token) {
+		res.status(401);
+		throw new Error('Not authorized, no token');
+	}
+});
+
+export { protect };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
+/**
+ * to implement middleware, place it as an argument on the request -> protect
+ */
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
