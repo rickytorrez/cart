21f3f19555695cf4fb7610628e74dd91cc3320b5
rickytorrez <torrez.ricky@ymail.com>
2021-04-03 19:25:01 -0400

    Header userInfo display

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { LinkContainer } from 'react-router-bootstrap';
 
 /** Container wraps everything and centers it on the screen */
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
+	const dispatch = useDispatch();
+
+	/**
+	 * extract userLogin slice of state to conditionally show on navbar
+	 */
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
+	const logoutHandler = () => {
+		console.log('object');
+	};
+
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -21,11 +35,23 @@ const Header = () => {
 									<i className='fas fa-shopping-cart' /> Cart
 								</Nav.Link>
 							</LinkContainer>
-							<LinkContainer to='/login'>
-								<Nav.Link>
-									<i className='fas fa-user' /> Sign In
-								</Nav.Link>
-							</LinkContainer>
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id='username'>
+									<LinkContainer to='/profile'>
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									{/* No link container since we have an event handler on the link itself */}
+									<NavDropdown.Item onClick={logoutHandler}>
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to='/login'>
+									<Nav.Link>
+										<i className='fas fa-user' /> Sign In
+									</Nav.Link>
+								</LinkContainer>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
