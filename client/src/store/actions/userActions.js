07379533cc47d3f0894c7641630a91e967d9891c
rickytorrez <torrez.ricky@ymail.com>
2021-04-05 21:50:55 -0400

    user register action

--- client/src/store/actions/userActions.js
+++ client/src/store/actions/userActions.js
@@ -4,6 +4,9 @@ import {
 	USER_LOGIN_REQUEST,
 	USER_LOGIN_SUCCESS,
 	USER_LOGOUT,
+	USER_REGISTER_FAIL,
+	USER_REGISTER_REQUEST,
+	USER_REGISTER_SUCCESS,
 } from '../constants/userConstants';
 
 /**
@@ -71,4 +74,71 @@ export const logout = () => async (dispatch) => {
 	dispatch({
 		type: USER_LOGOUT,
 	});
+};
+
+/**
+ * function that makes a server call for user registration
+ * @param {*} name
+ * @param {*} email
+ * @param {*} password
+ */
+export const register = (name, email, password) => async (dispatch) => {
+	try {
+		// dispatches the request to register
+		dispatch({
+			type: USER_REGISTER_REQUEST,
+		});
+
+		// content type for the request sent - headers
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		/**
+		 * to make a post request
+		 *  pass the route
+		 *  pass the data as a second arg on an object
+		 *  pass the config object for headers
+		 */
+		const { data } = await axios.post(
+			'/api/users',
+			{ name, email, password },
+			config
+		);
+
+		/**
+		 * when the request is completed
+		 *  dispatch the type of action for USER_REGISTER_SUCCESS
+		 *  for the payload, pass the data we get back from the request
+		 */
+		dispatch({
+			type: USER_REGISTER_SUCCESS,
+			payload: data,
+		});
+
+		/**
+		 * after user has registered, the application should add his token to session and log him in
+		 *  dispatch the type of action for USER_LOGIN_SUCCESS
+		 *  for the payload, pass the data we get back from the request
+		 */
+		dispatch({
+			type: USER_LOGIN_SUCCESS,
+			payload: data,
+		});
+
+		/**
+		 * set the user to local storage - user object
+		 */
+		localStorage.setItem('userInfo', JSON.stringify(data));
+	} catch (error) {
+		dispatch({
+			type: USER_REGISTER_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
 };
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,13 +7,14 @@ import {
 	productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer } from './reducers/userReducer';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducer';
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
 	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
 });
 
 /**
