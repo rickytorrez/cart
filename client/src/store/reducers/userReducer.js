23e1f65ab72724ae1bffe4256c052847976832ac
rickytorrez <torrez.ricky@ymail.com>
2021-04-08 18:57:27 -0400

    user details constants and reducer

--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -6,6 +6,9 @@ import {
 	USER_REGISTER_REQUEST,
 	USER_REGISTER_SUCCESS,
 	USER_REGISTER_FAIL,
+	USER_DETAILS_REQUEST,
+	USER_DETAILS_SUCCESS,
+	USER_DETAILS_FAIL,
 } from '../constants/userConstants';
 
 /**
@@ -41,6 +44,25 @@ export const userRegisterReducer = (state = {}, action) => {
 		case USER_REGISTER_SUCCESS:
 			return { loading: false };
 		case USER_REGISTER_FAIL:
+			return { loading: false, error: action.payload };
+		default:
+			return state;
+	}
+};
+
+/**
+ * handles the state for user details information
+ * @param {*} state empty user object as the initial state
+ * @param {*} action switches on case type
+ */
+export const userDetailsReducer = (state = { user: {} }, action) => {
+	switch (action.type) {
+		case USER_DETAILS_REQUEST:
+			// spreads state in case there's data in our state
+			return { ...state, loading: true };
+		case USER_DETAILS_SUCCESS:
+			return { loading: false, user: action.payload };
+		case USER_DETAILS_FAIL:
 			return { loading: false, error: action.payload };
 		default:
 			return state;
