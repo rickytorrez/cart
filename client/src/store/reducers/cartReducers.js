3d6930c1a15a19b54c7612bb900451dabbf5732b
rickytorrez <torrez.ricky@ymail.com>
2021-04-09 12:19:31 -0400

    address cart reducer

--- client/src/store/reducers/cartReducers.js
+++ client/src/store/reducers/cartReducers.js
@@ -1,4 +1,8 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+import {
+	CART_ADD_ITEM,
+	CART_REMOVE_ITEM,
+	CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstants';
 
 /**
  * cartReducer takes inital state, cartItems array for different and multiple items
@@ -61,6 +65,13 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 				cartItems: state.cartItems.filter(
 					(item) => item.product !== action.payload
 				),
+			};
+		case CART_SAVE_SHIPPING_ADDRESS:
+			return {
+				// spreads the state
+				...state,
+				// receives the data that we pass from the form as the action payload
+				shippingAddress: action.payload,
 			};
 		default:
 			return state;
