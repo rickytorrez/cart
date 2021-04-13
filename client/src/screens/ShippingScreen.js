6f6cbe5780dca31e1e43747e7809c441a09e9ebd
rickytorrez <torrez.ricky@ymail.com>
2021-04-12 21:51:47 -0400

    checkout breadcrumbs

--- client/src/screens/ShippingScreen.js
+++ client/src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveShippingAddress } from '../store/actions/cartActions';
 
+import CheckoutSteps from '../components/CheckoutSteps';
 import FormContainer from '../components/FormContainer';
 import { Form, Button } from 'react-bootstrap';
 
@@ -26,6 +27,7 @@ const ShippingScreen = ({ history }) => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				<Form.Group controlId='address'>
